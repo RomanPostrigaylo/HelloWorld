3f715b7bc422e0bf60345bef8e875902b8881ede
Roman <bbromka27@gmail.com>
2022-10-26 01:44:25 +0500

    add Logic

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var HelloWorldLabel: UILabel!
+    
+    @IBOutlet var toggleButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        HelloWorldLabel.isHidden = true
+        toggleButton.layer.cornerRadius = 11
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func makeButtonAction(_ sender: Any) {
+        if HelloWorldLabel.isHidden {
+            HelloWorldLabel.isHidden = false
+            toggleButton.setTitle("Hide TEXT", for: .normal)
+        } else {
+            HelloWorldLabel.isHidden = true
+            toggleButton.setTitle("Show TEXT", for: .normal)
+        }
+    }
+    
 
 }
 
